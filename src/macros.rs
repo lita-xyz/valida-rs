2c2d6e06354b304ac37dd0fd5372964fcaf268a5
thealmarty <thealmartyblog@gmail.com>
2024-10-28 08:40:52 -0700

    Cargo fmt.

--- src/macros.rs
+++ src/macros.rs
@@ -4,8 +4,8 @@ macro_rules! entrypoint {
         const DELENDUM_ENTRY: fn() = $path;
 
         mod delendum_generated_main {
-            use $crate::rand::delendum_rand;
             use $crate::getrandom::register_custom_getrandom;
+            use $crate::rand::delendum_rand;
 
             register_custom_getrandom!(delendum_rand);
 
