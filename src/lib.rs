10462969596a609d1e10777292c009043b2decb1
thealmarty <thealmartyblog@gmail.com>
2025-02-28 11:25:29 -0800

    Remove unneeded modules.

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,6 @@
 #![feature(once_cell_get_mut)]
-#![feature(custom_test_frameworks, test)]
-#![test_runner(test_utils::test_runner)]
 
 pub use getrandom;
 
-pub mod io;
 pub mod macros;
 pub mod rand;
-pub mod test_utils;
